1bccc2ba11b40df650bc8fca1e93cae1f6d0ff1a
Deimek <333olegovich333@gmail.com>
2024-12-04 15:38:39 +0100

    add_js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const myNamber = 10369;
+const newNumber = myNamber.toString().split('');
+// console.log(newNumber);
+const namberResalt = newNumber.join(' ');
+// console.log(typeof namberResalt);
+console.log(namberResalt);
+
